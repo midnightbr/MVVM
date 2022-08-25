6aa77e4157c70516fb8dd20375b305b793a8ac1e
marcos.henrique <marcos.henrique@engegraph.com.br>
2022-08-24 23:31:08 -0300

    Caliburn.Micro
    Primeiro projeto/aula para estudar o framework Caliburn.
    Infelizmente o exemplo não está concluído.
    Bug:
    - O UserControl não está trocando a informação da tela quando clicado no botão. Devido a estar em outra versão de dotnet e do framework do que da aula, 4.6 e 3.2 vs 6.0 e 4.0.173 respectivamente. Preciso procurar uma formar de resolver esse detalhe.

---
+++ CMDemo/WPFUI/Bootstrapper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Caliburn.Micro;
+using WPFUI.ViewModels;
+
+namespace WPFUI
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            // Obs.: Tive que usar o pacote de versão 4.0.173 inves do 4.0.210 pelo fato desse paramentro não ter mais suporte
+            // ao método DisplayRootViewFor, sendo alterado por um método asincrono (DisplayRootViewForAsync) no qual,
+            // não atualiza a tela instantaneamente.
+            // Para efeitos de estudo, vou manter a versão que tem suporte ao método usado no curso.
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
---
+++ CMDemo/WPFUI/ViewModels/ShellViewModel.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+using WPFUI.Models;
+
+namespace WPFUI.ViewModels
+{
+    public class ShellViewModel : Conductor<object>
+    {
+        private string _firstName;
+        private string _lastName;
+        private BindableCollection<PersonModel> _people = new BindableCollection<PersonModel>();
+        private PersonModel _selectedPerson;
+
+        public ShellViewModel()
+        {
+            People.Add(new PersonModel { FirstName = "John", LastName = "Lucca" });
+            People.Add(new PersonModel { FirstName = "Marcos", LastName = "Defaste" });
+            People.Add(new PersonModel { FirstName = "Davion", LastName = "Dragon" });
+            People.Add(new PersonModel { FirstName = "Fabiana", LastName = "Drumonte" });
+        }
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set
+            {
+                _firstName = value;
+                NotifyOfPropertyChange(() => FirstName);
+                NotifyOfPropertyChange(() => FullName);
+            }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set
+            {
+                _lastName = value;
+                NotifyOfPropertyChange(() => LastName);
+                NotifyOfPropertyChange(() => FullName);
+            }
+        }
+
+        public string FullName
+        {
+            get { return $"{FirstName} {LastName}"; }
+        }
+
+        public BindableCollection<PersonModel> People
+        {
+            get { return _people; }
+            set { _people = value; }
+        }
+
+        public PersonModel SelectedPerson
+        {
+            get { return _selectedPerson; }
+            set
+            {
+                _selectedPerson = value;
+                NotifyOfPropertyChange(() => SelectedPerson);
+            }
+        }
+
+        public bool CanClearText(string firstName, string lastName)
+        {
+            if (string.IsNullOrWhiteSpace(firstName) && string.IsNullOrWhiteSpace(lastName))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public void ClearText(string firstName, string lastName)
+        {
+            FirstName = "";
+            LastName = "";
+        }
+
+        public async void LoadPageOne()
+        {
+            await ActivateItemAsync(new FirstChildViewModel(), CancellationToken.None);
+        }
+
+        public async void LoadPageTwo()
+        {
+            await ActivateItemAsync(new SecondChildViewModel(), CancellationToken.None);
+        }
+    }
+}
